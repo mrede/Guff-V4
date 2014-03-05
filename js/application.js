c032a5938031367ef5102eb8951ff9cdb4f1c7c1
Ben Ede <ben.ede@gmail.com>
2014-03-05 19:31:25 +0000

    imported most stuff and removed old push notification code

--- js/application.js
+++ js/application.js
@@ -2,9 +2,6 @@ $("body").bind("pageAnimationEnd", function (e, info) {
     $('body').height($('div.current').height());
 });
 
-function Guff() {
-}
-
 var Guff = {
     loc: null,
     watchId: null,
@@ -16,7 +13,7 @@ var Guff = {
     accuracySteps: [],
     //url: '192.168.0.3:4567',
 
-    
+
 
     init: function() {
         //bind interactions - ******this should probably be moved till after we are happy with accuracy******
@@ -295,94 +292,15 @@ var Guff = {
         
     },
 
-    //Set up Token retrieval plugin
-    getTokenID: function(callback) {
-        if(typeof cordova.exec == 'function') { 
-            cordova.exec(callback, this.getTokenFail, "PushNotification", "getToken", []);
-        } else {
-            //Fake the callback
-            callback('12345')
-        }
-    },
-            
-    getTokenFail: function(err) {
-        alert("Failed to get Token")
-    },
-
-    receivedEvent: function(id) {
-        var pushNotification = window.plugins.pushNotification;
-        // TODO: Enter your own GCM Sender ID in the register call for Android
-        if (device.platform == 'android' || device.platform == 'Android') {
-            pushNotification.register(this.successHandler, this.errorHandler,{"senderID":"19707295672","ecb":"app.onNotificationGCM"});
-        }
-        else {
-            pushNotification.register(this.tokenHandler,this.errorHandler,{"badge":"true","sound":"true","alert":"true","ecb":"app.onNotificationAPN"});
-        }
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    },
-    // iOS
-    onNotificationAPN: function(event) {
-        var pushNotification = window.plugins.pushNotification;
-        console.log("Received a notification! " + event.alert);
-        console.log("event sound " + event.sound);
-        console.log("event badge " + event.badge);
-        console.log("event " + event);
-        if (event.alert) {
-            navigator.notification.alert(event.alert);
-        }
-        if (event.badge) {
-            console.log("Set badge on  " + pushNotification);
-            pushNotification.setApplicationIconBadgeNumber(this.successHandler, event.badge);
-        }
-        if (event.sound) {
-            var snd = new Media(event.sound);
-            snd.play();
-        }
-    },
-    // Android
-    onNotificationGCM: function(e) {
-        switch( e.event )
-        {
-            case 'registered':
-                if ( e.regid.length > 0 )
-                {
-                    // Your GCM push server needs to know the regID before it can push to this device
-                    // here is where you might want to send it the regID for later use.
-                    alert('registration id = '+e.regid);
-                }
-            break;
-
-            case 'message':
-              // this is the actual push notification. its format depends on the data model
-              // of the intermediary push server which must also be reflected in GCMIntentService.java
-              alert('message = '+e.message+' msgcnt = '+e.msgcnt);
-            break;
-
-            case 'error':
-              alert('GCM error = '+e.msg);
-            break;
-
-            default:
-              alert('An unknown GCM event has occurred');
-              break;
-        }
-    }
-
+    
 
 
 
 };
 
 $(function(){
-    var guff = new Guff();
-    guff.init();
+    //var guff = new Guff();
+    Guff.init();
 });
 
 var jQT = new $.jQTouch({
