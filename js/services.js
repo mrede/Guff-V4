bb07f7d6325fe95caff96d355164505b29be8fe9
Ben Ede <ben.ede@gmail.com>
2014-03-18 18:36:07 +0000

    removed (e) from call

--- js/services.js
+++ js/services.js
@@ -111,7 +111,7 @@ angular.module('starter.services', [])
           if ( device.platform == 'android' || device.platform == 'Android' )
           {
               alert("ANDROID");
-              pushNotification.register(app.pushRegisterSuccessHandler, app.pushRegisterErrorHandler,{"senderID":"507474617924","ecb":'angular.element(document.querySelector("#home")).scope().handlePushNotification(e)'});
+              pushNotification.register(app.pushRegisterSuccessHandler, app.pushRegisterErrorHandler,{"senderID":"507474617924","ecb":'angular.element(document.querySelector("#home")).scope().handlePushNotification'});
           } else {
               //IOS
               alert("Doing IOS");
